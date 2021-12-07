a3fa41086058592f1e1fbebe53b23e0a3938273a
a0z9 <a0z9@rambler.ru>
2021-12-07 17:27:19 +0300

    bridge

---
+++ ConsoleApplication2/App9.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+//bridge
+
+struct Implementor
+{
+	virtual void act() = 0;
+	//...
+};
+
+struct R1Implementor : Implementor
+{
+	void act() { cout << "R1 implementor: " << this << endl; }
+
+};
+
+struct Abstraction {
+
+	virtual void op() = 0;
+};
+
+
+struct RefinedAbstraction : Abstraction
+{
+public:
+	RefinedAbstraction(Implementor* impl) :implementor(impl) {}
+	void op() { implementor->act(); }
+	
+private:
+	Implementor* implementor;
+
+};
+
+
+
+int main() {
+
+	Implementor* i1 = new R1Implementor;
+	Abstraction* a1 = new RefinedAbstraction(i1);
+	a1->op();
+
+	delete i1;
+	delete a1;
+
+
+}
