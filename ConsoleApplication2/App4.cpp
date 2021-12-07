951bd15ff0ec0ed1b1b24692c0b28be413d49ce3
a0z9 <a0z9@rambler.ru>
2021-12-07 12:54:37 +0300

    Prototype-final

--- ConsoleApplication2/App4.cpp
+++ ConsoleApplication2/App4.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 
 using namespace std;
@@ -6,6 +7,36 @@ using namespace std;
 
 struct A { int val; int data; };
 struct B { int val; };
+
+struct Proto
+{
+	virtual Proto* clone() = 0;
+    //....
+};
+
+struct Object1 : Proto
+{
+	int data;
+	Object1();
+	Object1(int d) { data = d; }
+	//Object1(const Object1& o1);
+
+	Object1* clone() { return  new Object1(*this); }
+
+};
+
+Object1::Object1() { cout << "+++ ctor" << this; }
+
+
+void ProtoClone(Proto* proto)
+{
+	Proto* o2 = proto->clone();
+	unique_ptr<Proto> o3(proto->clone());
+	Proto& o4 = *proto->clone();
+
+	delete o2;
+
+}
 
 int main() {
 
@@ -16,5 +47,17 @@ int main() {
 
 	cout << a2.val << " -- "<< a2.data << endl;
 	cout << a1.val << " -- " << a1.data << endl;
+
+	//--------------------------
+
+	ProtoClone(new Object1(1));
+
+
+	//Object1 o1; o1.data = 1;
+
+	//Object1* o2 = o1.clone();
+
+
+
 
 }
